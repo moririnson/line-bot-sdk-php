bbaa64e2ff64e5608b5d0138664a89b1ecbeb3fd
moririnson <morimorim7180@gmail.com>
2020-06-09 21:30:05 +0900

    fix

--- src/LINEBot/HTTPClient/CurlHTTPClient.php
+++ src/LINEBot/HTTPClient/CurlHTTPClient.php
@@ -148,8 +148,10 @@ class CurlHTTPClient implements HTTPClient
         }
         if ($method === 'PUT') {
             if (!empty($reqBody)) {
+                $options[CURLOPT_PUT] = true;
                 $options[CURLOPT_POSTFIELDS] = json_encode($reqBody);
             } else {
+                $options[CURLOPT_PUT] = true;
                 $options[CURLOPT_POSTFIELDS] = $reqBody;
             }
         }
